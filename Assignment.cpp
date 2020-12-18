8468f2f1ac66c8bcba14c8b91fb8a4aa2299e4fb
hoatq-fptaptech <65893435+hoatq-fptaptech@users.noreply.github.com>
2020-12-18 18:00:20 +0700

    Create Assignment.cpp

---
+++ Assignment.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+bool timkiem(int x,int arr[],int n){
+	for(int i=0;i<n;i++){
+		if(x==arr[i])
+			return true;
+	}
+	return false;
+}
+void inSoLe(int arr[],int n){
+	for(int i=0;i<n;i++){
+		if(arr[i] %2!=0){
+			printf("%5d",arr[i]);
+		}
+	}
+}
+int timUCLN(int a,int b){
+	int ucln=1;
+	for(int i=1;i<=a;i++){
+		if(a%i==0&&b%i==0){
+			ucln= i;
+		}
+	}
+	return ucln;
+}
+int timBCNN(int a,int b){
+	for(int i=a;true;i++){
+		if(i%a==0&&i%b==0){
+			return i;
+		}
+	}
+}
+int main(){
+	
+}
